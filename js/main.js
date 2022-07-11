7ef8c097505fb5e81b126a303adc7b0c474061c2
saramonzon22 <sara.mon.quesada@gmail.com>
2022-07-11 16:51:52 +0100

    added some fix and reset button

--- js/main.js
+++ js/main.js
@@ -8,44 +8,71 @@ const btn = document.querySelector('.try');
 const inputNumber = document.querySelector('.input-number');
 const clue = document.querySelector('.clue-js');
 const numberTry = document.querySelector('.try-number');
+const reset = document.querySelector('.reset');
 
+let contar = 0;
 
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
+function userNumber(){
+    userNumberInput = inputNumber.value; 
+}
+
 let randomNumber = getRandomNumber(100);
     console.log(randomNumber);
 
 
-function guess (event){
-    event.preventDefault();
-    const whatNumber = inputNumber.value;
+function guess (){
     
-    if (whatNumber == randomNumber ){
-        clue.innerHTML = '¡¡¡Has ganado campeona!!!'
+    const whatNumber = parseInt(inputNumber.value);
+    
+    if (isNaN(whatNumber)) {
+        clue.innerHTML = 'El número debe estar entre 1 y 100.'
     }
-    else if (whatNumber < randomNumber){
+    else if (whatNumber < 1){
+        clue.innerHTML = 'Pista: El número debe estar entre 1 y 100'
+    }
+    else if (whatNumber > 100){
+        clue.innerHTML = 'El número debe estar entre 1 y 100.'
+    }
+      else if (whatNumber < randomNumber){
         clue.innerHTML = 'Pista: Demasiado bajo.'
     }
     else if (whatNumber > randomNumber){
         clue.innerHTML = 'Pista: Demasiado alto.'
     }
-    if (whatNumber > 100){
-        clue.innerHTML = 'El número debe estar entre 1 y 100.'
-    }
-    if (whatNumber < 0){
-        clue.innerHTML = 'El número debe estar entre 1 y 100.'
+    else if (whatNumber === ''){
+        clue.innerHTML = 'Pista: Introduce un número entre 1 y 100.'
     }
     
-};
+    else if (whatNumber === randomNumber){
+        clue.innerHTML = '¡¡¡Has ganado campeona!!!'}
+    
+    };
 
-let contar = 0;
-btn.onclick = function(){
-    contar++;
-    numberTry.innerHTML = contar;
+  
+function count(){
+        btn.onclick = function(){
+        contar++;
+        numberTry.innerHTML = contar;
+    }    
 }
 
+function handleClick(event){
+    event.preventDefault();
+    count();
+    guess();
+    
+}
 
-btn.addEventListener('click', guess);
+function resetClick(){
+    contar = 0;
+    numberTry.innerHTML = 0;
+}
+
+reset.addEventListener('click', resetClick);
+btn.addEventListener('click', handleClick);
+/* btn.addEventListener('click', guess); */
